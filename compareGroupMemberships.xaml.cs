03a16e767631e0e2f0ef47cc21e93650b3d7baa2
oldManLemon <oldm8reggie@gmail.com>
2019-11-12 16:24:07 +0100

    working with the CSV files we have created

--- compareGroupMemberships.xaml.cs
+++ compareGroupMemberships.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Shapes;
 using System.Management.Automation;
 using System.Collections.ObjectModel;
 using System.Management.Automation.Runspaces;
+using LumenWorks.Framework.IO.Csv;
+using System.IO;
+using System.Data;
+using Microsoft.VisualBasic.FileIO;
 
 namespace poshScripts
 {
@@ -40,8 +44,22 @@ namespace poshScripts
      
 
         private void loadLeft_Click(object sender, RoutedEventArgs e)
+
         {
+            var path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"\\aviovaManagementApp\\usrData\\"+server+".csv");
             var leftUser = UsrLeft.Text;
+            DataTable dt = new DataTable();
+            using(TextFieldParser parser = new TextFieldParser(path))
+            {
+                // set the parser variables
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+            }
+            //using(CsvReader csv = new CsvReader(
+            //    new StreamReader(path), true))
+            //{
+            //    csv.ToLookup("ahase");
+            //}
             //Initialise Powershell
             InitialSessionState iss = InitialSessionState.CreateDefault2();
             var shell = PowerShell.Create(iss);
@@ -54,19 +72,19 @@ namespace poshScripts
                 var res = shell.Invoke();
                 if (res.Count > 0)
                 {
-                    var builder = new StringBuilder();
+                    //var builder = new StringBuilder();
                     foreach (var psObject in res)
                     {
-                        builder.Append(psObject.BaseObject.ToString() + "\r\n");
+                        GroupListLeft.Items.Add(psObject.BaseObject.ToString());
                     }
-                    GroupListLeft.Items.Add(builder.ToString());
                 }
-            }catch(Exception err)
+                TotalLeft.Content = res.Count;
+            }
+            catch(Exception err)
             {
                 MessageBox.Show(err.Message);
             }
         }
-        
 
         private void reloadUsers(object sender, RoutedEventArgs e)
         {
@@ -81,31 +99,40 @@ namespace poshScripts
             try
             {
                 //GroupListLeft
-               
-
                 var res = shell.Invoke();
-                //resultsBox.Text += res.Count;
-                //resultsBox.Text += res.ToString();
-                // resultsBox.Text += "\n";
-                //if (res.Count > 0)
-                //{
-                //    var builder = new StringBuilder();
-                //    foreach (var psObject in res)
-                //    {
-                //        try
-                //        {
-                //            builder.Append(psObject.BaseObject.ToString() + "\r\n");
-                //        }
-                //        catch { builder.Append("ObjNull\r\n"); }
-                //    }
-                
-                //    GroupListLeft.Items.Add(builder.ToString());
-                //}
-
             }
             catch (Exception Err)
             {
                 MessageBox.Show(Err.Message);
+            }
+        }
+
+        private void loadRight_Click(object sender, RoutedEventArgs e)
+        {
+            var rightUser = UsrRight.Text;
+            //Initialise Powershell
+            InitialSessionState iss = InitialSessionState.CreateDefault2();
+            var shell = PowerShell.Create(iss);
+            var script = "C:\\Users\\ahase\\source\\repos\\poshScripts\\scritps\\getUsrGroup.ps1";
+            shell.Commands.AddCommand(script);
+            shell.Commands.AddArgument(rightUser);
+            shell.Commands.AddArgument(server);
+            try
+            {
+                var res = shell.Invoke();
+                if (res.Count > 0)
+                {
+                    //var builder = new StringBuilder();
+                    foreach (var psObject in res)
+                    {
+                        GroupListRight.Items.Add(psObject.BaseObject.ToString());
+                    }
+                }
+                TotalRight.Content = res.Count;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
             }
         }
     }
